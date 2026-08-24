a730d5cacd8c6cbbee50ec8a81387bc55b2e79fb
nikhil0006kumar-ux <nikhil0006kumar@gmail.com>
2026-08-24 22:59:24 +0530

    Add files via upload

---
+++ Chapter 4/01_loop.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    // Problem Print Happy Birthday 1 Lakh times 
+    printf("Happy Birtdhay!\n");
+    return 0;
+}
---
+++ Chapter 4/06_inrement_decrement.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    int i = 5;
+     printf("The value of i is %d\n", i);
+
+    i = i + 5; // 10
+    printf("The value of i is %d\n", i);
+   
+    printf("The value of i is %d\n", i++); // 10
+    printf("The value of i is %d\n", i); // 11
+
+
+    i +=2; // same as i = 1 + 2;
+    printf("The value of i is %d\n", i); // 13
+
+
+    // i++ prints i first and then increment i (Posty increment operator)
+    // ++i increment i first and then prints i (Posty increment operator)
+    return 0;
+}
