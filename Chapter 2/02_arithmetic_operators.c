a68d0a5c168d3075e1718ef4d062060f7f4eb6eb
nikhil0006kumar-ux <nikhil0006kumar@gmail.com>
2026-08-24 22:54:13 +0530

    Add files via upload

---
+++ Chapter 2/02_arithmetic_operators.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int a = 6;
+    int b = 3;
+    int c = a + b;
+    printf("The value of a is %d and value of b is %d and sum is %d\n", a, b, c);
+    // Modulus operator is used to get the remainder
+    printf("the remainder when a is divided by is: %d\n", a%b);
+
+    // This does not work for exponentiation in c 
+    // int d = a^b;
+    
+    
+    return 0;
+
+}
+
---
+++ Chapter 2/05_typecasting.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int n = 45;
+    float m = 32.23;
+
+    n = (int) m; // convert the date type to int
+    printf("%d/n", n);
+    return 0;
+
+   
+}
+
