3ecd3451c1031a6bda02fe492e2d815e3b895348
nikhil0006kumar-ux <nikhil0006kumar@gmail.com>
2026-08-24 22:53:29 +0530

    Add files via upload

---
+++ Chapter 1 practice set/02_problem2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int r = 6;
+    int height = 10;
+    printf("The area of circle with radius %d is %f\n", r, 3.14*r*r);
+        printf("Volume of cylinder with radius %d height %d is %f", r, height, 3.14*r*r*height);
+    return 0;
+}
+
